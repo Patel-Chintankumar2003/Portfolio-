25070a4eda90dace1b9b394cf43f40ebb0a551ea
Ashura <ashuramajestic@gmail.com>
2024-09-18 22:27:11 +0530

    Login error

--- Backend/app.js
+++ Backend/app.js
@@ -1,12 +1,19 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import path from "path";
+import cors from "cors";
 export const app = express();
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cookieParser());
+const corsOptions = {
+  origin: "http://localhost:5173",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+};
 
+app.use(cors(corsOptions));
 import { userRouter } from "./routes/User.js";
 app.use("/api/v1", userRouter);
 
